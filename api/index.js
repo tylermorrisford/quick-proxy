dd17dd46251e2a0442a6dc8f9644a0364d232770
Tyler M Ford <tylerford@gmail.com>
2023-11-20 16:25:46 -0700

    update proxy

--- api/index.js
+++ api/index.js
@@ -1,3 +1,20 @@
-export default async function handler(req, res) {
-  res.status(200).send(`Hello world!`);
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const apiProxy = createProxyMiddleware({
+  target: 'https://api-web.nhle.com/v1/',
+  changeOrigin: true,
+  hostRewrite: true,
+  methodRewrite: true,
+  followRedirects: true,
+  pathRewrite: {
+    '^/api': '',
+  },
+});
+
+export default function (req, res) {
+  return apiProxy(req, res);
 }
+
+// export default async function handler(req, res) {
+//   res.status(200).send(`Hello world!`);
+// }
